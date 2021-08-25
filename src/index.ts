ed1c270616f549d8a76d7cafd8a9ea203bf95c28
leoc200 <22406910+leocth@users.noreply.github.com>
2021-08-25 21:50:57 +0800

    :help_me:

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import * as BABYLON from "babylonjs"
+import "./index.css";
+
+// main canvas
 const canvas = document.createElement("canvas")
 canvas.style.width = "100%";
 document.body.append(canvas);
-const Engine = new BABYLON.Engine(canvas, true)
-const Scene = new BABYLON.Scene(Engine)
-Scene.render()
+
+const engine = new BABYLON.Engine(canvas, true);
+const scene = new BABYLON.Scene(engine);
+
+engine.runRenderLoop(() => {
+    scene.render();
+});
