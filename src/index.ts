f82700610f4322c2c13fab3f6b53cc273201340f
leoc200 <22406910+leocth@users.noreply.github.com>
2021-08-25 22:01:58 +0800

    use the proper way with `Engine#runRenderLoop`

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
-import * as gl from "babylonjs"
+import * as BABYLON from "babylonjs"
+import "./index.css"
 
 const canvas = document.createElement("canvas")
-canvas.style.width = "100%";
-canvas.width = 2000;
-canvas.height = 1000;
 document.body.append(canvas);
 
-const engine = new gl.Engine(canvas)
-// await engine.initAsync();
+const engine = new BABYLON.Engine(canvas);
+const scene = new BABYLON.Scene(engine)
 
-const scene = new gl.Scene(engine)
-
-var camera = new gl.FreeCamera("camera1", new gl.Vector3(0, 10, 10), scene);
-camera.setTarget(gl.Vector3.Zero());
+var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 10, 10), scene);
+camera.setTarget(BABYLON.Vector3.Zero());
 camera.attachControl(canvas, true);
 
-const light = new gl.HemisphericLight("light", new gl.Vector3(0, 1, 0), scene);
+const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
 
-const box = gl.MeshBuilder.CreateBox("box", {}, scene);
+const box = BABYLON.MeshBuilder.CreateBox("box", {}, scene);
 
 setInterval(() => {
-    box.rotate(new gl.Vector3(0, 0, .001), 3)
-    scene.render();
+    box.rotate(new BABYLON.Vector3(0, 0, .001), 3);
 }, 50)
-scene.render()
+
+engine.runRenderLoop(() => {
+    scene.render();
+})
+
+window.addEventListener("resize", function () {
+    engine.resize();
+});
